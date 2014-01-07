dabb525ae56775bf01c6fe864b2a4151f3b537e7
lijingpeng <lijingpeng@outlook.com>
2014-01-07 23:17:14 +0800

    exp finish no dot

--- exponentiation.cc
+++ exponentiation.cc
@@ -6,95 +6,94 @@
 
 #include <iostream>
 #include <string.h>
+#include <string>
 #include <stdlib.h>
 using namespace std;
-typedef int IntegerType;
 
 const int MAX = 1024;
-int multiply(const char* A, const char* B, int* res)
+void rexpn(string r, const char* b, char* res)
 {
-    unsigned int ALen = strlen(A);
-    unsigned int BLen = strlen(B);
-    //int* res = (int*)malloc( sizeof(int*) * (ALen + BLen) );
-    //if(res)
-    //    memset(res, 0, ALen+BLen);
-    //else
-    //    return -1;
+    //1找小数点
+    int dotPos = 0;
+    dotPos = r.find('.');
+    dotPos = dotPos == -1 ? 0: dotPos;
+    
+    //2删除小数点并删除无效的0
+    string pureNum("");
+    int endPos = 0;
+    if(dotPos) // has dot
+    {
+        string tmp = r.substr(0, dotPos);
+        if(tmp[0] != '0')
+            pureNum += tmp;
 
-    // Multiply
-    for(int i = 0; i < ALen; i++)
-        for(int j = 0; j < BLen; j++)
+        tmp = r.substr(dotPos + 1);
+        int tmpLen = tmp.length();
+        endPos = tmpLen;
+        for(int i=tmpLen - 1; i >= 0; i--)
         {
-            res[ i + j + 1] += (A[i] - '0') * (B[j] - '0');
+            if(tmp[i] == '0')
+                endPos = i;
+            else
+                break;
+        }
+        pureNum += tmp.substr(0, endPos);
+     }
+     else
+        pureNum = r;
+     
+     //3计算乘法
+      
+    int rLen = pureNum.length();
+    int bLen = strlen(b);
+    int intRes[MAX*MAX+1];
+    memset(intRes, 0, MAX*MAX+1);
+    for(int i = 0; i < rLen; i++)
+        for(int j = 0; j < bLen; j++)
+        {
+            intRes[ i + j + 1] += (pureNum[i] - '0') * (b[j] - '0');
         }
     
-    for(int i  = ALen + BLen - 1; i >= 0; i--)
+    for(int i  = rLen + bLen - 1; i >= 0; i--)
     {
-        res[i - 1] += res[i] / 10;
-        res[i] = res[i] % 10;
+        intRes[i - 1] += intRes[i] / 10;
+        intRes[i] = intRes[i] % 10;
+        res[i] = intRes[i] + '0' ;
     }
-
-    return 0;
+/*
+    string resStr("");
+    sringstream ss;
+	string tmp("");
+    for(int i = 0; i < ALen+BLen; i++)
+    {
+	if(res[0] == 0)
+	{
+		continue;
+	}
+	ss.clear();
+	tmp = "";
+	ss << res[i];
+	ss >> tmp;
+	resStr += tmp;
+    }*/
+    return;
 }
+char res[MAX * MAX + 1];
 int main()
 {
-    string r, n;
+    string r;
+    int n;
     while(cin >> r >> n)
     {
-        const char *nTmp = n.c_str();
-        int dotPos = 0;
-        dotPos = r.find('.');
-        dotPos = dotPos == -1 ? 0: dotPos;
-        int rLen = r.length();
-
-        string pureNum("");
-        int endPos = 0;
-        if(dotPos) // has dot
-        {
-            string tmp = r.substr(0, dotPos);
-            if(tmp[0] != '0')
-                pureNum += tmp;
-
-            tmp = r.substr(dotPos + 1);
-            int tmpLen = tmp.length();
-            endPos = tmpLen;
-            for(int i=tmpLen - 1; i >= 0; i--)
-            {
-                if(tmp[i] == '0')
-                    endPos = i;
-                else
-                    break;
-            }
-            pureNum += tmp.substr(0, endPos);
-            //if()
-        }
-        else
-            pureNum = r;
-        cout << pureNum << " " << n << endl;
-        //cout << dotPos << endl;
-
-        int nNum = atoi(nTmp);
-        unsigned int pureNumLen = pureNum.length();
-        int*  res = (int*)calloc(2 * pureNumLen, sizeof(int*));
-        char* resChar = (char*)calloc(pureNumLen, sizeof(char));
-        memcpy(resChar, pureNum.c_str(), pureNumLen);
-        for(int i = 2; i < nNum; i = i * 2)
-        {     
-            res = (int*)realloc(res, sizeof(int*) * ( pureNumLen * i) );
-            if(res)
-                memset(res, 0, pureNumLen * i);
-            else
-                return -1;
-
-            multiply(resChar, resChar, res);
-            resChar = (char*)realloc(resChar, i * pureNumLen * sizeof(char));
-            for(int j=0; j< pureNumLen * i; j++)
-            {
-                resChar[j] = res[j] + '0';
-                cout << resChar[j];
-            }
-            cout<<endl;
-        }
+       memset(res, 0, MAX*MAX+1);
+       rexpn(r, r.c_str(), res);
+       //cout << res << endl;
+       //memset(res, 0, MAX*MAX+1);
+       for(int i = 2; i < n; i++)
+       {
+            rexpn(r, res, res);
+       }
+       cout << res << endl;
     }
     return 0;
-}
+}   
