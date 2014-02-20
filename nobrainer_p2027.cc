2ea35cbf03012eef3716d72ca7836fe232acf253
lijingpeng <lijingpeng@outlook.com>
2014-02-20 11:41:04 +0800

    2027

---
+++ nobrainer_p2027.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if(a < b)
+            cout << "NO BRAINS" << endl;
+        else
+            cout << "MMM BRAINS" << endl;
+    }
+    return 0;
+}
