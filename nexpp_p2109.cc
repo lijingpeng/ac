9691102fab7f0d54a67eb2ca5becb2fd5add9497
lijingpeng <lijingpeng@outlook.com>
2014-02-22 16:38:29 +0800

    2109

---
+++ nexpp_p2109.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double n, p;
+    while(cin >> n >> p)
+    {
+        cout << pow(p, 1.0/n) << endl;
+    }
+    return 0;
+}
