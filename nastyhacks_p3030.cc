6dae5dcdf025cd82dba6fe1842f3b639e142ac47
lijingpeng <lijingpeng@outlook.com>
2014-02-25 18:27:45 +0800

    3030

---
+++ nastyhacks_p3030.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, r, e, c;
+    while(cin >> n && n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            cin >> r >> e >> c;
+            if(e - c > r)
+                cout << "advertise" << endl;
+            else if(e - c == r)
+                cout << "does not matter" << endl;
+            else
+                cout << "do not advertise" << endl;
+        }
+    }
+    return 0;
+}
