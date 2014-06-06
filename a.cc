3cb69190dbcf78e349f23b703b7cb58d3d550081
lijingpeng <lijingpeng@outlook.com>
2014-06-06 20:05:37 +0800

    a

--- a.cc
+++ a.cc
@@ -1,18 +1,13 @@
-/**
- * common header
- */
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int n;
-    while(cin >> n && n)
+    int t;
+    cin >> t;
+    while( t-- )
     {
-        for(int i = 0; i < n; i++)
-        {
-            
-        }
+
     }
     return 0;
 }
